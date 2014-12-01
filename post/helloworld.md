d205d3dffeeac31bfe559d632236578c00735dbc
lichaozheng <lcz@ejoy.com>
2014-12-01 14:38:19 +0800

    hello world

---
+++ post/helloworld.md
@@ -0,0 +1,19 @@
+#Hello World
+
+又又又重新开了博客。
+
+事情是这样的，两年前用wordpress搭建了一个博客，虽然一直没有写，但域名一直在续费，服务器一直在买。又由于wordpress是php+mysql的，我对这二者一窍不通，每次迁移都折腾的要死。后来觉得同事[lvzixun](https://github.com/lvzixun)的方法不错，文章用[markdown](https://help.github.com/articles/markdown-basics/)写，放在github上，然后写个脚本自动拉到服务器上转成html页面。于是我也这么撸了一个。
+
+wordpress的文章可以导出成xml，然后用python解析一下导过来应该很容易。还想把之前所有博客都导过来，网上找到有人写了一个[blogstowordpress](http://www.crifan.com/crifan_released_all/website/python/blogstowordpress/)脚本，试用了一下发现有些博客不成功，比如QQ空间，以后再折腾吧。
+
+##为什么不用Jekyll？
+Jekyll是ruby写的，跟php一样都是不懂，我想要一个自己可以完全控制的，想用python。
+
+##为什么是纯静态的？
+开始想做动态的，去了解了一下python的web开发，才知道要用[框架](https://wiki.python.org/moin/WebFrameworks)，而看起来python的部署比php更复杂（我就是怕迁移麻烦才放弃wordpress的）。于是还是先搭一个静态站点，目前也足够用了，后面有精力再改。
+
+##为什么不能评论？
+静态站也可以使用第三方的评论系统。看了下[多说](http://duoshuo.com/)和[Disqus](https://disqus.com/)，准备用多说，因为有分享到国内SNS的功能，另外就是应该有敏感词过滤吧，我不想因为谁的一句评论让网站被墙掉。这篇文章目前还不能评论，近期会加上。
+
+##为什么这么丑？
+你才丑。
---
+++ reload.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import os
+import time
+import urllib
+import json
+import re
+
+def md_to_html(md):
+	data = {"text": md, "mode": "markdown"}
+	params = json.dumps(data).encode("utf-8")
+	handle = urllib.urlopen("https://api.github.com/markdown", params)
+	return handle.read()
+
+POST_RE = re.compile("^#([^\n]+)")
+def get_metainfo(md):
+	m = POST_RE.match(md)
+	if m:
+		return m.group(1)
+
+class BlogPost(object):
+	def __init__(self, path):
+		self.target_path = "blog/%s.html" % os.path.basename(path).split(".")[0]
+		self.create_time = time.localtime(os.path.getctime(path))
+
+		fp = open(path, "r")
+		md = fp.read()
+		self.title = get_metainfo(md)
+		self.html = md_to_html(md)
+
+	def write_html(self, path):
+		template = open("template/blog.html", "r").read()
+		html = template.replace("-TITLE-", self.title).replace("-CONTENT-", self.html)
+		open(path + self.target_path, "w").write(html)
+
+	def get_index_html(self):
+		template = "<span class=\"blog-date\">%s - </span><a href=\"%s\">%s</a>"
+		date = time.strftime("%d %b", self.create_time)
+		return template % (date, self.target_path, self.title)
+
+def main():
+	all_post = []
+
+	for fn in os.listdir("post"):
+		p = os.path.join("post", fn)
+		if os.path.isdir(p): continue
+		if p.startswith("."): continue
+		if not p.endswith(".md"): continue
+
+		post = BlogPost(p)
+		all_post.append(post)
+
+	all_post.sort(key=lambda x: x.create_time, reverse=True)
+	cur_year = 0
+	tags = []
+	for post in all_post:
+		year = post.create_time.tm_year
+		if year != cur_year:
+			if cur_year != 0:
+				tags.append("</ul>")
+			tags.append("<h2>%d</h2>" % year)
+			tags.append("<ul>")
+			cur_year = year
+		tags.append("<li>%s</li>" % post.get_index_html())
+		post.write_html("site/")
+	tags.append("</ul>")
+
+	template = open("template/index.html", "r").read()
+	html = template.replace("-BLOG-", "".join(tags))
+	open("site/index.html", "w").write(html)
+
+if __name__ == "__main__":
+	main()
